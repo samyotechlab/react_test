062861de28543725c0e72e4321d488ad78a5fd6c
Apple <abhi@samyotech.com>
2021-08-03 13:52:35 +0530

    update repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [firstCount, setFirstCount] = React.useState(0);
+  const [secCount, setSecCount] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Test</h1>
+      <textarea type="text" rows={5} onChange={e => setFirstCount(e.target.value.length)}></textarea>
+      <p>Text Count { firstCount }</p>
+      <textarea type="text" rows={5} onChange={e => setSecCount(e.target.value.length)}></textarea>
+      <p>Text Count { secCount }</p>
     </div>
   );
 }
